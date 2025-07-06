ee4a336716518401e4f782e7aed3e4966e11744e
Jorge Luiz <jre-jl@hotmail.com>
2025-07-06 15:57:13 -0300

    trabalhando com conjuntos - sets

---
+++ escola_v2_com_sets.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Exibe relatório de crianças por atividades.
+
+Imprimir a lista de crianças agrupadas por sala 
+que frequentam cada uma das atividades.
+"""
+__version__ = "0.1.1"
+
+# Dados
+sala1 = ["Erik", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"]
+sala2 = ["Joao", "Antonio", "Carlos", "Maria", "Isolda"]
+
+aula_ingles = ["Erik", "Maia", "Joana", "Carlos", "Antonio"]
+aula_musica = ["Erik", "Carlos", "Maria"]
+aula_danca = ["Gustavo", "Sofia", "Joana", "Antonio"]
+
+atividades = [
+    ("Inglês", aula_ingles),
+    ("Música", aula_musica),
+    ("Dança", aula_danca),
+]
+
+# Listar alunos em cada atividade por sala
+
+for nome_atividade, atv in atividades:
+    print("{:^40}".format(f"Alunos na atividade {nome_atividade}:"))
+    print("-" * 40)
+
+    atv_sala1 = set(sala1) & set(atv)
+    atv_sala2 = set(sala2).intersection(atv)
+
+    print("Sala 1: ", atv_sala1)
+    print("Sala 2: ", atv_sala2)
+
+    print()
+    print("#" * 40)
