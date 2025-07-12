dafa59f2c164e112932fdf2a454bed45ef7cf4ce
Jorge Luiz <jre-jl@hotmail.com>
2025-07-12 10:29:08 -0300

    trabalhando com dicionários

--- hello.py
+++ hello.py
@@ -26,16 +26,26 @@ import os
 # sempre no padrão snack case
 current_language = os.getenv("LANG", "en_US")[:5]   # usando fatiamento em python
 
-msg = "Hello, World!"
+msg = {
+    "en_US": "Hello, World!",
+    "pt_BR": "Olá, Mundo!",
+    "it_IT": "Ciao, Mondo!",
+    "es_ES": "Hola, Mundo!",
+    "fr_FR": "Bonjour, Monde!",
+}
 
-if current_language == "pt_BR":
-    msg = "Olá Mundo!"
-elif current_language == "it_IT":
-    msg = "Ciao, Mondo!"
-elif current_language = "es_SP":
-    msg = "Hola, Mundo!"
-elif current_language == "fr_FR":
-    msg = "Bonjour Monde!"
+print(msg[current_language])
+
+# msg = "Hello, World!"
+# 
+# if current_language == "pt_BR":
+#     msg = "Olá Mundo!"
+# elif current_language == "it_IT":
+#     msg = "Ciao, Mondo!"
+# elif current_language = "es_SP":
+#     msg = "Hola, Mundo!"
+# elif current_language == "fr_FR":
+#     msg = "Bonjour Monde!"
 
 # print é uma biblioteca built-in
-print(msg)   
+# print(msg)   
