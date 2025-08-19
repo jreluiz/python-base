768c444c4031bea0113e875f41007f223388abf6
Jorge Luiz <jre-jl@hotmail.com>
2025-08-19 11:46:32 -0300

    trabalhando com logs

--- logs.py
+++ logs.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python3
 
+import os
 import logging
+from logging import handlers
+
+# TODO: usar função
+# TODO: usar a lib loguru
+
+log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
 
 #instância
-log = logging.Logger("logs.py", logging.DEBUG)
+log = logging.Logger("logs.py", log_level)
 
 #level
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+#ch = logging.StreamHandler()  # manda para o console/terminal/stderr
+#ch.setLevel(log_level)
+fh = handlers.RotatingFileHandler(  # salvar log em arquivo
+    "meu_log.log",
+    maxBytes=300,    # interessante usar 10**6 = 1MB
+    backupCount=10,  # cria até 10 arquivos
+)
 
 #formatacao
 fmt = logging.Formatter("%(asctime)s %(name)s %(levelname)s l:%(lineno)d f:%(filename)s: %(message)s")
-ch.setFormatter(fmt)
+#ch.setFormatter(fmt)
+fh.setFormatter(fmt)
 
 #destino
-log.addHandler(ch)
+#log.addHandler(ch)
+log.addHandler(fh)
 
 log.debug("Mensagem para o dev")
 log.info("Mensagens gerais para os usuários")
@@ -22,4 +36,7 @@ log.warning("Aviso que não causa erros")
 log.error("Erro que afeta uma única execução")
 log.critical("Erro geral, por ex banco sumiu")
 
-
+try:
+    1 / 0
+except ZeroDivisionError as e:
+    log.error("Deu erro %s", str(e))
